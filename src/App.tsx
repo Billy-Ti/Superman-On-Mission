97e29331587661378309a42aec6e45de09cf5f28
Billy Ti <78161308+Billy-Ti@users.noreply.github.com>
2023-11-16 14:09:42 +0800

    Init :seedling: Personal project init and test project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Home from "./pages/Home/index.tsx";
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import StartTask from "../components/StartTask";
+
+const index = () => {
+  return (
+    <>
+      <div className="container mx-auto">
+        <p className="text-center mt-10 text-xl">Home</p>
+        <StartTask />
+      </div>
+    </>
+  );
+};
+
+export default index;
