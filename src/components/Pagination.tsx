f723aa84a003f566ae29350362a71b6831a9cc37
Billy Ti <78161308+Billy-Ti@users.noreply.github.com>
2023-11-25 16:10:39 +0800

    Feat ✨ Completed of the page display task function 1. Completing the pagination UI Screen 2. Display the total number of tasks according to the number of tasks 3. When user clicks "Previous" or "Next" on the first and last page, an alert will pop up 4. Dynamically display up to three pages according to different page numbers

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,82 @@
+import { showAlert } from "../utils/showAlert";
+
+interface PaginationProps {
+  tasksPerPage: number;
+  totalTasks: number;
+  paginate: (pageNumber: number) => void;
+  currentPage: number;
+}
+const Pagination: React.FC<PaginationProps> = ({
+  tasksPerPage,
+  totalTasks,
+  paginate,
+  currentPage,
+}) => {
+  const totalPages = Math.ceil(totalTasks / tasksPerPage);
+
+  const pageNumbers = [];
+  let startPage = currentPage - 1 <= 0 ? 1 : currentPage - 1;
+
+  // 設定當前頁為 1 或 2 的情況
+  if (currentPage <= 2) {
+    startPage = 1;
+  } else if (currentPage > 2 && currentPage < totalPages) {
+    // 如果當前頁碼在中間時，保證左邊有一個頁碼，右邊有兩個頁碼 (含 Next)
+    startPage = currentPage - 1;
+  } else if (currentPage === totalPages) {
+    // 處理當前頁碼為最後一頁的情況
+    startPage = totalPages - 2 <= 0 ? 1 : totalPages - 2;
+  }
+
+  for (let i = startPage; i <= Math.min(startPage + 2, totalPages); i++) {
+    pageNumbers.push(i);
+  }
+
+  const handlePreviousPageButton = () => {
+    if (currentPage > 1) {
+      paginate(currentPage - 1);
+    } else {
+      showAlert("🚨系統提醒", "Hi 已經是第一頁囉", "info");
+    }
+  };
+  const handleNextPageButton = () => {
+    if (currentPage < totalPages) {
+      paginate(currentPage + 1);
+    } else {
+      showAlert("🚨系統提醒", "Hi 已經是最後一頁囉", "info");
+    }
+  };
+
+  return (
+    <div className="flex justify-center">
+      <nav className="flex space-x-2" aria-label="Pagination">
+        <a
+          onClick={handlePreviousPageButton}
+          className="cursor-pointer rounded-md bg-gradient-to-r from-violet-300 to-indigo-300 px-4 py-2 text-white"
+        >
+          Previous
+        </a>
+        {pageNumbers.map((number) => (
+          <a
+            key={number}
+            onClick={() => paginate(number)}
+            className={`cursor-pointer rounded-md px-4 py-2 ${
+              currentPage === number
+                ? "bg-fuchsia-200"
+                : "bg-white text-gray-700"
+            }`}
+          >
+            {number}
+          </a>
+        ))}
+        <a
+          onClick={handleNextPageButton}
+          className="cursor-pointer rounded-md bg-gradient-to-r from-violet-300 to-indigo-300 px-4 py-2 text-white"
+        >
+          Next
+        </a>
+      </nav>
+    </div>
+  );
+};
+export default Pagination;
