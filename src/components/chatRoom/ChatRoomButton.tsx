1e1762519f030bf640d3ef19027d4715957ab629
Billy Ti <78161308+Billy-Ti@users.noreply.github.com>
2023-11-30 17:09:41 +0800

    Update ⬆️ Update the chat room’s red text reminder for unread messages 1. Red text will be displayed when there is a new message 2. When the user is clicked, the prompt text will be cancelled 3. Pending : Use a more conspicuous method on the web page to remind the user that there are new messages

--- src/components/chatRoom/ChatRoomButton.tsx
+++ src/components/chatRoom/ChatRoomButton.tsx
@@ -1,27 +1,41 @@
-import { Icon } from "@iconify/react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { collection, query, where, onSnapshot, getFirestore } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import ChatRoomWindow from "./ChatRoomWindow";
+import { Icon } from "@iconify/react";
 
 const ChatRoomButton = () => {
   const [isChatOpen, setIsChatOpen] = useState(false);
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [newMessageCount, setNewMessageCount] = useState(0);
   const navigate = useNavigate();
   const auth = getAuth();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setIsUserLoggedIn(!!user);
+      if (user) {
+        // 用戶已登錄，監聽新消息
+        const firestore = getFirestore();
+        const messagesRef = collection(firestore, "messages");
+        const q = query(
+          messagesRef,
+          where("sentTo", "==", user.uid), // 根據您的數據模型調整此條件
+          where("isRead", "==", false)
+        );
+        const unsubscribeMessages = onSnapshot(q, (snapshot) => {
+          setNewMessageCount(snapshot.docs.length);
+        });
+        return () => unsubscribeMessages();
+      }
     });
     return () => unsubscribe();
   }, [auth]);
 
   const handleChatButtonClick = () => {
-    if (isUserLoggedIn) {
+    if (auth.currentUser) {
       setIsChatOpen(true);
     } else {
-      navigate("/SignIn"); // 導航到登入頁面
+      navigate("/SignIn");
     }
   };
 
@@ -29,18 +43,15 @@ const ChatRoomButton = () => {
     <div className="fixed right-[1%] top-1/2">
       <button type="button" onClick={handleChatButtonClick}>
         <div className="w-50 h-50 flex items-center justify-center rounded-full bg-gradient-to-r from-blue-500 to-purple-400">
-          <Icon
-            icon="uiw:message"
-            color="white"
-            width="50"
-            height="50"
-            hFlip={true}
-          />
+          <Icon icon="uiw:message" color="white" width="50" height="50" hFlip={true} />
+          {newMessageCount > 0 && (
+            <span className="absolute -right-2 -top-2 flex h-6 w-6 items-center justify-center rounded-full bg-red-500 text-xs text-white">
+              !
+            </span>
+          )}
         </div>
       </button>
-      {isChatOpen && (
-        <ChatRoomWindow onCloseRoom={() => setIsChatOpen(false)} />
-      )}
+      {isChatOpen && <ChatRoomWindow onCloseRoom={() => setIsChatOpen(false)} />}
     </div>
   );
 };
