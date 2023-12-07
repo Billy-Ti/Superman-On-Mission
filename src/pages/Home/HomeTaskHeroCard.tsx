72bfe9e18b8634f5d81c677201ed28a9df053285
Billy Ti <78161308+Billy-Ti@users.noreply.github.com>
2023-12-07 10:18:23 +0800

    Fix 🔧 Correct synchronization layout of other forums

--- src/pages/Home/HomeTaskHeroCard.tsx
+++ src/pages/Home/HomeTaskHeroCard.tsx
@@ -11,6 +11,11 @@ const HomeTaskHeroCard: React.FC = () => {
 
   return (
     <div className="container mx-auto max-w-[1280px] px-4 pt-4 md:px-20">
+      <div className="mb-24 px-4 text-center md:px-0 lg:text-left">
+        <p className="inline bg-gradient-to-r from-blue-700 via-blue-500 to-purple-400 bg-clip-text text-4xl font-black italic text-transparent">
+          Excellent Tasker
+        </p>
+      </div>
       <div className="relative mb-20 select-none">
         <div className="container relative mx-[auto] my-[0] h-[200px] w-[250px] [perspective:1000px]">
           <div
